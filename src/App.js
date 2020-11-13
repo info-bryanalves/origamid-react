29c441a913c4b0d3994f351bc4c3f92d255856b7
Bryan Alves <=>
2020-11-13 00:15:03 -0300

    Aula 0207 - JSX 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import React from 'react'
 
 const App = () => {
-  return <div>App React</div>;
+
+  const users = [
+    {
+      name: "Bryan Alves",
+      age: "25",
+    },
+    {
+      name: "Ana Salles",
+      age: "24",
+    },
+    {
+      name: "Giovana Salles",
+      age: "15",
+    },
+  ]
+
+  const path = 'https://www.origamid.com/'
+
+  return (
+    <>
+      <h1>App React</h1>
+      <a href={path} target="_blank">Origamid</a>
+      {
+        users.map(({ name, age }) => {
+          return (
+            <>
+              <p>Nome: {name}</p>
+              <p>Idade: {age}</p>
+              <p>Maioridade: {age >= 18 ? 'Maior' : 'Menor'}</p>
+            </>
+          )
+        })}
+    </>
+  );
 };
 
 export default App;
