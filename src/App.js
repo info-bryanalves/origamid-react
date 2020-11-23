770cb109807f2a946934050059fdf81b5329adb4
Bryan Alves <=>
2020-11-23 12:01:32 -0300

    Aula 0208 - JSX Arrays 2

--- src/App.js
+++ src/App.js
@@ -3,18 +3,42 @@ import React from 'react'
 const App = () => {
 
   const products = [
-    { name: 'Smart TV', price: 2599.00 },
-    { name: 'Notebook', price: 3650.00 },
-    { name: 'Relógio', price: 450.00 },
+    {
+      id: 1,
+      name: 'Smartphone',
+      price: 'R$ 2000',
+      colors: ['#29d8d5', '#252a34', '#fc3766'],
+    },
+    {
+      id: 2,
+      name: 'Notebook',
+      price: 'R$ 3000',
+      colors: ['#ffd045', '#d4394b', '#f37c59'],
+    },
+    {
+      id: 3,
+      name: 'Tablet',
+      price: 'R$ 1500',
+      colors: ['#365069', '#47c1c8', '#f95786'],
+    },
   ]
 
   return (
-    <div>
-      {products.filter(({ price }) => price > 500)
-        .map(({ name, price }) => (
-          <li key={name}>{name} - R$ {price}</li>
-        ))}
-    </div>
+    <section>
+      {products
+      .filter(({price}) => Number(price.replace('R$','')) > 1500)
+      .map(({ id, name, price, colors }) => (
+        <div key={id}>
+          <h1>{name}</h1>
+          <p>Preço: {price}</p>
+          <ul>
+            {colors.map((item) => (
+              <li key={item} style={{ backgroundColor: item, color: 'white' }}>{item}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </section>
   )
 
 }
