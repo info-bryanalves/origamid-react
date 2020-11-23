653c684543f0fc9ca77c94a2257340c5e2aab821
Bryan Alves <=>
2020-11-23 11:47:00 -0300

    Aula 0208 - JSX Arrays 1

--- src/App.js
+++ src/App.js
@@ -1,53 +1,22 @@
 import React from 'react'
 
-const ana = {
-  name: 'Ana',
-  age: 27,
-  shopping: [
-    { name: 'Notebook', price: 'R$ 2500' },
-    { name: 'Geladeira', price: 'R$ 3000' },
-    { name: 'Smartphone', price: 'R$ 1500' },
-  ],
-  status: true,
-}
-
-const bryan = {
-  name: 'Bryan',
-  age: 25,
-  shopping: [
-    { name: 'Notebook', price: 'R$ 2500' },
-    { name: 'Geladeira', price: 'R$ 3000' },
-    { name: 'Smartphone', price: 'R$ 1500' },
-    { name: 'Guitarra', price: 'R$ 3500' },
-  ],
-  status: false,
-};
-
 const App = () => {
-  const { name, age, shopping, status } = ana;
 
-  const values = shopping.map(({ price }) => {
-    return Number(price.replace('R$', ''))
-  })
-
-  const total = values.reduce((accumulator, currentValue) => accumulator + currentValue)
+  const products = [
+    { name: 'Smart TV', price: 2599.00 },
+    { name: 'Notebook', price: 3650.00 },
+    { name: 'Relógio', price: 450.00 },
+  ]
 
   return (
-    <div style={{ fontWeight: "500" }}>
-      <p>Nome: {name}</p>
-      <p>Idade: {age}</p>
-      <p>
-        Situação:{' '}
-        <span style={{color: status ? 'green' : 'red',}}>
-          {status ? 'Ativa' : 'Inativa'}
-        </span>
-      </p>
-      <p>Total gasto: {total}</p>
-      {total >= 10000 &&
-        <p>Você está gastando demais</p>
-      }
+    <div>
+      {products.filter(({ price }) => price > 500)
+        .map(({ name, price }) => (
+          <li key={name}>{name} - R$ {price}</li>
+        ))}
     </div>
-  );
-};
+  )
 
-export default App;
+}
+
+export default App
